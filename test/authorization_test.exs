0f338824bac89cfaf201e0f0bc350cd67c315e72
Jarro Put <put.jarro@gmail.com>
2016-07-01 20:55:30 +0200

    Fix test assertion

--- test/authorization_test.exs
+++ test/authorization_test.exs
@@ -4,7 +4,7 @@ defmodule SpotifyAuthorizationTest do
 
   test "#call with scopes" do
     Application.put_env(:spotify_ex, :scopes, ["foo", "bar"])
-    assert(Spotify.Authorization.call == "https://accounts.spotify.com/authorize?client_id=#{Spotify.client_id}&response_type=code&redirect_uri=#{Spotify.callback_url}&scopes=foo+bar")
+    assert(Spotify.Authorization.call == "https://accounts.spotify.com/authorize?client_id=#{Spotify.client_id}&response_type=code&redirect_uri=#{Spotify.callback_url}&scope=foo%20bar")
   end
 
   test "#call without scopes" do
