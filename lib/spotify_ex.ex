fcd4173d45bb78ccabb5c5740aeef34fa298e0f9
Jarro Put <put.jarro@gmail.com>
2016-07-02 19:11:21 +0200

    Remove unused function

--- lib/spotify_ex.ex
+++ lib/spotify_ex.ex
@@ -18,15 +18,4 @@ defmodule Spotify do
       {"User-agent", "Spotify_ex"}
     ]
   end
-
-  # https://groups.google.com/forum/#!msg/elixir-lang-talk/6geXOLUeIpI/L9einu4EEAAJ
-  def to_struct(kind, attrs) do
-    struct = struct(kind)
-    Enum.reduce Map.to_list(struct), struct, fn {k, _}, acc ->
-      case Map.fetch(attrs, Atom.to_string(k)) do
-        {:ok, v} -> %{acc | k => v}
-        :error -> acc
-      end
-    end
-  end
 end
